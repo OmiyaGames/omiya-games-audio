d97d3d793521807b446da1ef78ab33f28021ca92
Taro Omiya <taro.omiya@omiyagames.com>
2022-02-16 01:27:48 -0600

    Adding proper implementations for AudioSettingsBuilder and AudioSettingsEditor.

--- Editor/AudioSettingsBuilder.cs
+++ Editor/AudioSettingsBuilder.cs
@@ -1,21 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using OmiyaGames.Global.Settings.Editor;
 
 namespace OmiyaGames.Audio.Editor
 {
-    public class AudioSettingsBuilder : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-    }
+	///-----------------------------------------------------------------------
+	/// <remarks>
+	/// <copyright file="AudioSettingsBuilder.cs" company="Omiya Games">
+	/// The MIT License (MIT)
+	/// 
+	/// Copyright (c) 2022 Omiya Games
+	/// 
+	/// Permission is hereby granted, free of charge, to any person obtaining a copy
+	/// of this software and associated documentation files (the "Software"), to deal
+	/// in the Software without restriction, including without limitation the rights
+	/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	/// copies of the Software, and to permit persons to whom the Software is
+	/// furnished to do so, subject to the following conditions:
+	/// 
+	/// The above copyright notice and this permission notice shall be included in
+	/// all copies or substantial portions of the Software.
+	/// 
+	/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	/// THE SOFTWARE.
+	/// </copyright>
+	/// <list type="table">
+	/// <listheader>
+	/// <term>Revision</term>
+	/// <description>Description</description>
+	/// </listheader>
+	/// <item>
+	/// <term>
+	/// <strong>Version:</strong> 1.0.0-pre.1<br/>
+	/// <strong>Date:</strong> 2/16/2022<br/>
+	/// <strong>Author:</strong> Taro Omiya
+	/// </term>
+	/// <description>Initial verison.</description>
+	/// </item>
+	/// </list>
+	/// </remarks>
+	///-----------------------------------------------------------------------
+	/// <summary>
+	/// Pre- and post-build script to add an instance of
+	/// <see cref="AudioSettings"/> into the pre-loaded assets (and thus, build.)
+	/// </summary>
+	public class AudioSettingsBuilder : BaseSettingsBuilder<AudioSettings>
+	{
+		/// <inheritdoc/>
+		public override string ConfigName => AudioManager.CONFIG_NAME;
+	}
 }
