2cf527ee010328d1ae408382dbff8b4f8bafd7d9
Taro Omiya <taro.omiya@omiyagames.com>
2022-03-13 03:15:09 -0500

    Significantly simplifying AudioSettings.Reset() method by simply copying the member variables from an existing AudioSettings.

--- Runtime/AudioSettings.cs
+++ Runtime/AudioSettings.cs
@@ -126,133 +126,77 @@ namespace OmiyaGames.Audio
 			return base.OnUpgrade(oldVersion, out errorMessage);
 		}
 
+		/// <summary>
+		/// The default percent to decibels converter,
+		/// assuming mute-decibel is -80 dB.
+		/// </summary>
+		/// <param name="percent">
+		/// Value from 0 to 1.
+		/// </param>
+		/// <returns>
+		/// Volume, in decibels, where -80 dB is mute.
+		/// </returns>
+		public static float DefaultPercentToVolumeDbConversion(float percent)
+		{
+			const float MIN_PERCENT_CLAMP = 0.0001f;
+			percent = Mathf.Clamp(percent, MIN_PERCENT_CLAMP, 1f);
+			return Mathf.Log10(percent) * 20;
+		}
+
 #if UNITY_EDITOR
 		void Reset()
 		{
 			const string DATA_DIRECTORY = "Packages/com.omiyagames.audio/Runtime/Data/";
-			const string DEFAULT_MIXER_PATH = DATA_DIRECTORY + "DefaultMixer.mixer";
-			const float DEFAULT_FADE_DURATION = 0.25f;
+			const string DEFAULT_SETTINGS_PATH = DATA_DIRECTORY + "Audio Settings - Default.asset";
 			const int NUM_KEYFRAMES = 15;
 
-			const string MAIN_VOLUME_PATH = DATA_DIRECTORY + "MainVolumeSetting.asset";
-			const string MAIN_MUTE_PATH = DATA_DIRECTORY + "MainMuteSetting.asset";
+			// Grab the default settings
+			AudioSettings defaultSettings = UnityEditor.AssetDatabase.LoadAssetAtPath<AudioSettings>(DEFAULT_SETTINGS_PATH);
 
-			const string MUSIC_VOLUME_PATH = DATA_DIRECTORY + "MusicVolumeSetting.asset";
-			const string MUSIC_MUTE_PATH = DATA_DIRECTORY + "MusicMuteSetting.asset";
-
-			const string SFX_VOLUME_PATH = DATA_DIRECTORY + "SoundEffectsVolumeSetting.asset";
-			const string SFX_MUTE_PATH = DATA_DIRECTORY + "SoundEffectsMuteSetting.asset";
-
-			const string VOICES_VOLUME_PATH = DATA_DIRECTORY + "VoicesVolumeSetting.asset";
-			const string VOICES_MUTE_PATH = DATA_DIRECTORY + "VoicesMuteSetting.asset";
-
-			const string AMBIENCE_VOLUME_PATH = DATA_DIRECTORY + "AmbienceVolumeSetting.asset";
-			const string AMBIENCE_MUTE_PATH = DATA_DIRECTORY + "AmbienceMuteSetting.asset";
-
-			// Setup mixer
-			mixer = UnityEditor.AssetDatabase.LoadAssetAtPath<AudioMixer>(DEFAULT_MIXER_PATH);
-
-			// Setup main layer
-			main.VolumeParam = "Main Volume";
-			main.PitchParam = "Main Pitch";
-			main.VolumeSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveFloat>(MAIN_VOLUME_PATH);
-			main.IsMutedSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveBool>(MAIN_MUTE_PATH);
-
-			// Setup music layer
-			music.VolumeParam = "Music Volume";
-			music.PitchParam = "Music Pitch";
-			music.VolumeSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveFloat>(MUSIC_VOLUME_PATH);
-			music.IsMutedSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveBool>(MUSIC_MUTE_PATH);
-			music.DefaultGroup = mixer.FindMatchingGroups("World Music")[0];
-			music.DefaultFadeDurationSeconds = DEFAULT_FADE_DURATION;
-			music.FadeGroups = GetFadeGroups(Mixer, "Fade Music");
-
-			// Setup sound effects layer
-			soundEffects.VolumeParam = "Sound Effects Volume";
-			soundEffects.PitchParam = "Sound Effects Pitch";
-			soundEffects.VolumeSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveFloat>(SFX_VOLUME_PATH);
-			soundEffects.IsMutedSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveBool>(SFX_MUTE_PATH);
-			soundEffects.DefaultGroup = mixer.FindMatchingGroups("World Sound Effects")[0];
-			soundEffects.DefaultUiGroup = mixer.FindMatchingGroups("UI Sound Effects")[0];
-
-			// Setup voices layer
-			voices.VolumeParam = "Voices Volume";
-			voices.PitchParam = "Voices Pitch";
-			voices.VolumeSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveFloat>(VOICES_VOLUME_PATH);
-			voices.IsMutedSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveBool>(VOICES_MUTE_PATH);
-			voices.DefaultGroup = mixer.FindMatchingGroups("World Voices")[0];
-			voices.DefaultUiGroup = mixer.FindMatchingGroups("UI Voices")[0];
-
-			// Setup ambience layer
-			ambience.VolumeParam = "Ambience Volume";
-			ambience.PitchParam = "Ambience Pitch";
-			ambience.VolumeSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveFloat>(AMBIENCE_VOLUME_PATH);
-			ambience.IsMutedSaver = UnityEditor.AssetDatabase.LoadAssetAtPath<SaveBool>(AMBIENCE_MUTE_PATH);
-			ambience.DefaultGroup = mixer.FindMatchingGroups("World Ambience")[0];
-			ambience.DefaultFadeDurationSeconds = DEFAULT_FADE_DURATION;
-			ambience.FadeGroups = GetFadeGroups(Mixer, "Fade Ambience");
-
-			// Setup the keyframes
+			// Copy all the member variables
+			mixer = defaultSettings.mixer;
+			
+			muteVolumeDb = defaultSettings.muteVolumeDb;
 			percentToDbCurve = CreateDefaultCurve(NUM_KEYFRAMES);
 
-			static AudioMixerGroup[] GetFadeGroups(AudioMixer mixer, string prepend)
+			main = defaultSettings.main;
+			music = defaultSettings.music;
+			soundEffects = defaultSettings.soundEffects;
+			voices = defaultSettings.voices;
+			ambience = defaultSettings.ambience;
+
+			timeScaleSnapshots = defaultSettings.timeScaleSnapshots;
+		}
+
+		static AnimationCurve CreateDefaultCurve(int numberOfKeyFrames)
+		{
+			const float REDUCTION_MULTIPLIER = 0.65f;
+
+			// Generate the first keyframe (at time 0)
+			Keyframe[] defaultKeyframes = new Keyframe[numberOfKeyFrames];
+			defaultKeyframes[0] = new(0, DefaultPercentToVolumeDbConversion(0));
+
+			// Generate the rest (1 through almost 0)
+			float time = 1f;
+			for (int i = 1; i < defaultKeyframes.Length; ++i)
 			{
-				const int NUM_GROUPS = 3;
-				AudioMixerGroup[] toReturn = new AudioMixerGroup[NUM_GROUPS];
-				System.Text.StringBuilder builder = new(prepend.Length + 2);
+				defaultKeyframes[i] = new(time, DefaultPercentToVolumeDbConversion(time));
+				defaultKeyframes[i].weightedMode = WeightedMode.None;
 
-				// Find all groups
-				for (int i = 0; i < NUM_GROUPS; ++i)
-				{
-					// Generate name of group
-					builder.Clear();
-					builder.Append(prepend);
-					builder.Append(' ');
-					builder.Append(i + 1);
-
-					// Find this group
-					toReturn[i] = mixer.FindMatchingGroups(builder.ToString())[0];
-				}
-				return toReturn;
+				// Calculate the next time
+				time *= REDUCTION_MULTIPLIER;
 			}
 
-			static AnimationCurve CreateDefaultCurve(int numberOfKeyFrames)
+			// Apply keyframes to the curve
+			AnimationCurve returnCurve = new(defaultKeyframes);
+
+			// Change tangent mode on all the keyframes
+			for (int i = 0; i < numberOfKeyFrames; ++i)
 			{
-				const float REDUCTION_MULTIPLIER = 0.65f;
-
-				// Generate the first keyframe (at time 0)
-				Keyframe[] defaultKeyframes = new Keyframe[numberOfKeyFrames];
-				defaultKeyframes[0] = new(0, DefaultPercentToVolumeDbConversion(0));
-
-				// Generate the rest (1 through almost 0)
-				float time = 1f;
-				for (int i = 1; i < defaultKeyframes.Length; ++i)
-				{
-					defaultKeyframes[i] = new(time, DefaultPercentToVolumeDbConversion(time));
-					defaultKeyframes[i].weightedMode = WeightedMode.None;
-
-					// Calculate the next time
-					time *= REDUCTION_MULTIPLIER;
-				}
-
-				// Apply keyframes to the curve
-				AnimationCurve returnCurve = new(defaultKeyframes);
-
-				// Change tangent mode on all the keyframes
-				for (int i = 0; i < numberOfKeyFrames; ++i)
-				{
-					UnityEditor.AnimationUtility.SetKeyLeftTangentMode(returnCurve, i, UnityEditor.AnimationUtility.TangentMode.ClampedAuto);
-					UnityEditor.AnimationUtility.SetKeyRightTangentMode(returnCurve, i, UnityEditor.AnimationUtility.TangentMode.ClampedAuto);
-				}
-				return returnCurve;
-
-				static float DefaultPercentToVolumeDbConversion(float percent)
-				{
-					const float MIN_PERCENT_CLAMP = 0.0001f;
-					percent = Mathf.Clamp(percent, MIN_PERCENT_CLAMP, 1f);
-					return Mathf.Log10(percent) * 20;
-				}
+				UnityEditor.AnimationUtility.SetKeyLeftTangentMode(returnCurve, i, UnityEditor.AnimationUtility.TangentMode.ClampedAuto);
+				UnityEditor.AnimationUtility.SetKeyRightTangentMode(returnCurve, i, UnityEditor.AnimationUtility.TangentMode.ClampedAuto);
 			}
+			return returnCurve;
 		}
 #endif
 	}
