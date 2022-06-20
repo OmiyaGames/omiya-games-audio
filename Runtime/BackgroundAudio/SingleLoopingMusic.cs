40365f7fd1fada050021bb38e587f4e862dd7147
Taro Omiya <taro.omiya@omiyagames.com>
2022-06-19 22:48:01 -0500

    Moving `SingleLoopingMusicPlayer.cs` code back into `SingleLoopingMusic.cs`, since it's confirmed to work just fine.  Fixing `SingleLoopingMusicPlayer.State` property to return Scheduled correctly.

--- Runtime/BackgroundAudio/SingleLoopingMusic.cs
+++ Runtime/BackgroundAudio/SingleLoopingMusic.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace OmiyaGames.Audio
 {
@@ -49,15 +50,14 @@ namespace OmiyaGames.Audio
 	[CreateAssetMenu(menuName = "Omiya Games/Audio/Looping Music", fileName = "Looping Music", order = (MENU_ORDER))]
 	public class SingleLoopingMusic : BackgroundAudio
 	{
-		// FIXME: remove the public from all of these member variables
 		[SerializeField]
-		private AudioClip loop;
+		AudioClip loop;
 
 		[Header("Intro Stings")]
 		[SerializeField]
-		private AudioClip introSting;
+		AudioClip introSting;
 		[SerializeField]
-		private double playLoopAfterSeconds;
+		double playLoopAfterSeconds;
 
 		/// <summary>
 		/// TODO
@@ -94,7 +94,6 @@ namespace OmiyaGames.Audio
 		/// This produces a more accurate value than
 		/// <see cref="AudioClip.length"/>.
 		/// </summary>
-		[ContextMenu("Calculate Intro Sting Duration")]
 		public void SetLoopDelayToIntroStingDuration()
 		{
 			// Make sure we have a sting to play
@@ -102,6 +101,234 @@ namespace OmiyaGames.Audio
 			{
 				// Calculate the duration based on samples and frequency
 				playLoopAfterSeconds = AudioManager.CalculateClipLengthSeconds(IntroSting);
+			}
+		}
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		class SingleLoopingMusicPlayer : Player
+		{
+			[SerializeField]
+			SingleLoopingMusic data;
+			[SerializeField]
+			AudioSource intro;
+			[SerializeField]
+			AudioSource loop;
+			[SerializeField]
+			double playTimeStamp = 0;
+
+			/// <inheritdoc/>
+			public override event ITrackable<PlayState>.ChangeEvent OnBeforeChangeState;
+			/// <inheritdoc/>
+			public override event ITrackable<PlayState>.ChangeEvent OnAfterChangeState;
+
+			/// <inheritdoc/>
+			public override BackgroundAudio Data => data;
+			/// <inheritdoc/>
+			public override PlayState State
+			{
+				get
+				{
+					// Check if either Audio sources are playing
+					if (((loop != null) && (loop.isPlaying)) ||
+						((intro != null) && (intro.isPlaying)))
+					{
+						// Check if it's before the scheduled time of play
+						if (playTimeStamp > UnityEngine.AudioSettings.dspTime)
+						{
+							return PlayState.Scheduled;
+						}
+						return PlayState.Playing;
+					}
+					return PlayState.Stopped;
+				}
+			}
+			/// <inheritdoc/>
+			public override AudioMixerGroup MixerGroup
+			{
+				set
+				{
+					// Setup the audio groups
+					loop.outputAudioMixerGroup = value;
+					if (intro != null)
+					{
+						intro.outputAudioMixerGroup = value;
+					}
+				}
+			}
+
+			/// <inheritdoc/>
+			public override void Play(PlaybackArgs args)
+			{
+				// Grab the state changes
+				PlayState startState = State;
+				PlayState endState = PlayState.Playing;
+
+				// Determine when to start playing the audio
+				playTimeStamp = UnityEngine.AudioSettings.dspTime;
+				double skipForwardToSeconds = 0;
+				if (args != null)
+				{
+					skipForwardToSeconds = args.SkipForwardToSeconds;
+
+					if (args.DelaySeconds > 0)
+					{
+						endState = PlayState.Scheduled;
+						playTimeStamp += args.DelaySeconds;
+					}
+				}
+
+				// Check the state
+				if (startState == PlayState.Stopped)
+				{
+					// Invoke state change event
+					OnBeforeChangeState?.Invoke(startState, endState);
+
+					PlayFromStop(skipForwardToSeconds);
+
+					// Invoke state change event
+					OnAfterChangeState?.Invoke(startState, endState);
+				}
+			}
+
+			/// <inheritdoc/>
+			public override void Stop()
+			{
+				PlayState startState = State;
+				if (startState != PlayState.Stopped)
+				{
+					// Invoke state change event
+					OnBeforeChangeState?.Invoke(startState, PlayState.Stopped);
+
+					// Stop both audio sources immediately
+					if (loop != null)
+					{
+						loop.Stop();
+					}
+					if (intro != null)
+					{
+						intro.Stop();
+					}
+
+					// Invoke state change event
+					OnAfterChangeState?.Invoke(startState, PlayState.Stopped);
+				}
+			}
+
+			/// <inheritdoc/>
+			//public override void Pause()
+			//{
+			//	PlayState startState = State;
+			//	if ((startState == PlayState.Playing) || (startState == PlayState.Scheduled))
+			//	{
+			//		// Invoke state change event
+			//		OnBeforeChangeState?.Invoke(startState, PlayState.Paused);
+
+			//		// Pause main audio source
+			//		if (intro != null)
+			//		{
+			//			// TODO: figure out the times and scheduled delays so resuming is easier
+			//			// don't forget to reset these stats on Stop()
+			//			throw new System.NotImplementedException();
+			//			intro.Pause();
+			//			loop.Pause();
+			//		}
+			//		else
+			//		{
+			//			loop.Pause();
+			//		}
+
+			//		// Invoke state change event
+			//		OnAfterChangeState?.Invoke(startState, PlayState.Paused);
+			//	}
+			//}
+
+			/// <inheritdoc/>
+			internal void Setup(SingleLoopingMusic data)
+			{
+				// Make sure audio prefab is not null
+				if (data.MainAudioSourcePrefab == null)
+				{
+					throw new System.ArgumentNullException(nameof(data), "Cannot create a new audio source without a prefab.");
+				}
+
+				// Setup the data
+				this.data = data;
+
+				// Setup the looping audio player
+				loop = null;
+				if (data.Loop)
+				{
+					loop = Instantiate(Data.MainAudioSourcePrefab, Vector3.zero, Quaternion.identity, transform);
+					loop.gameObject.name = "Looping Audio Player";
+					loop.loop = true;
+					loop.clip = data.Loop;
+				}
+
+				// Check if we need to generate the intro sting player
+				intro = null;
+				if (data.IntroSting)
+				{
+					intro = Instantiate(data.MainAudioSourcePrefab, Vector3.zero, Quaternion.identity, transform);
+					intro.gameObject.name = "Intro Sting Player";
+					intro.loop = false;
+					intro.clip = data.IntroSting;
+				}
+			}
+
+			void PlayFromStop(double skipForwardToSeconds)
+			{
+				// Check if there's an intro sting to play
+				if (intro != null)
+				{
+					// Confirm skipping forward timestamp doesn't exceed intro sting length
+					double introLength = AudioManager.CalculateClipLengthSeconds(intro.clip);
+					if (skipForwardToSeconds < introLength)
+					{
+						// If not, schedule playing the intro
+						intro.PlayScheduled(playTimeStamp);
+						intro.timeSamples = AudioManager.CalculateTimeSample(intro.clip, skipForwardToSeconds);
+
+						// Delay playing the main tune
+						playTimeStamp += data.PlayLoopAfterSeconds;
+
+						// Update skip forward timestamp for main loop
+						skipForwardToSeconds -= data.PlayLoopAfterSeconds;
+					}
+					else
+					{
+						// Don't play intro sting, and skip main-loop forward by a certain amount
+						skipForwardToSeconds -= introLength;
+					}
+				}
+
+				// Check if there's a loop to play
+				if (loop != null)
+				{
+					// Schedule when to play the main loop
+					loop.PlayScheduled(playTimeStamp);
+
+					// Correct skip forward time for main loop
+					if (skipForwardToSeconds < 0)
+					{
+						skipForwardToSeconds = 0;
+					}
+					else
+					{
+						// Calculate how long the main loop is
+						double mainLength = AudioManager.CalculateClipLengthSeconds(loop.clip);
+
+						// Reduce skip forward duration by this number until it's small enough
+						while (skipForwardToSeconds > mainLength)
+						{
+							skipForwardToSeconds -= mainLength;
+						}
+					}
+
+					// Skip main forward in time
+					loop.timeSamples = AudioManager.CalculateTimeSample(loop.clip, skipForwardToSeconds);
+				}
 			}
 		}
 	}
